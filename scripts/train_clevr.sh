f7533c0f4fc6ff56ef2cc790df3b9474440672ec
XiongWenye <bearxwy@163.com>
2025-11-07 19:51:32 -0500

    Update for resume checkpoints

--- scripts/train_clevr.sh
+++ scripts/train_clevr.sh
@@ -16,6 +16,7 @@ export WANDB_API_KEY="4b9dc043dac87d50c74128f48c230b1755f954ea"
 
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --multi_gpu --num_processes=2 \
 --main_process_port 28100 train_clevr.py --enable_xformers_memory_efficient_attention \
+--resume_from_checkpoint=latest \ 
 --dataloader_num_workers 4 --learning_rate 2e-5 --mixed_precision fp16 --num_validation_images 10 \
 --val_batch_size 10 --max_train_steps 500000 --checkpointing_steps 10000 --checkpoints_total_limit 1 \
 --gradient_accumulation_steps 1 --seed 42 \
