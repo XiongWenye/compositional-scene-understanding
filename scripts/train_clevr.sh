81e06c9a2c1b7c076f1eb127afa2dc4cb3136581
XiongWenye <bearxwy@163.com>
2025-11-09 10:32:23 -0500

    Fix a minor bug in train srcipt

--- scripts/train_clevr.sh
+++ scripts/train_clevr.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1  
 #SBATCH --ntasks-per-node=1  
 #SBATCH --cpus-per-task=8  
-#SBATCH --gres=gpu:2  
+#SBATCH --gres=gpu:tesla_v100-pcie-32gb:2  
 #SBATCH --time=48:00:00  
 #SBATCH --mem=64G  
 
@@ -16,9 +16,9 @@ export WANDB_API_KEY="4b9dc043dac87d50c74128f48c230b1755f954ea"
 
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --multi_gpu --num_processes=2 \
 --main_process_port 28100 train_clevr.py --enable_xformers_memory_efficient_attention \
---resume_from_checkpoint=latest \ 
+--resume_from_checkpoint=latest \
 --dataloader_num_workers 4 --learning_rate 2e-5 --mixed_precision fp16 --num_validation_images 10 \
---val_batch_size 10 --max_train_steps 500000 --checkpointing_steps 10000 --checkpoints_total_limit 1 \
+--val_batch_size 10 --max_train_steps 100000 --checkpointing_steps 5000 --checkpoints_total_limit 1 \
 --gradient_accumulation_steps 1 --seed 42 \
 --output_dir ./outputs/ \
 --scheduler_config configs/clevr-2D-pos/scheduler/scheduler_config.json \
